bfa9208369b1ca55c39d42c90164f3a1c6b2cf3c
a20012251 <a20012251@gmail.com>
2015-12-07 16:11:08 -0500

    Update setup_master.sh

--- setup_master.sh
+++ setup_master.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-su - hduser
+# using hduser
 ssh-keygen -t rsa -P "" -f /home/hduser/.ssh/id_rsa
 for i in host1 host2 host3 localhost; do ssh-copy-id $i; done
 # password is 12341234
