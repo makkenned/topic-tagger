da2d4966e0cd0890bd4cb74954a05b13e8062caa
a20012251 <a20012251@gmail.com>
2015-12-07 16:31:24 -0500

    Update setup_master.sh

--- setup_master.sh
+++ setup_master.sh
@@ -2,4 +2,16 @@
 
 # using hduser
 ssh-keygen -t rsa -P "" -f /home/hduser/.ssh/id_rsa
-for i in host1 host2 host3 localhost; do ssh-copy-id $i; done
+for i in host2 host3 host4 host5 host6 host7 host8 host9 localhost; do ssh-copy-id $i; done
+
+cat <<EOT >/usr/local/hadoop/etc/hadoop/
+host2
+host3
+host4
+host5
+host6
+host7
+host8
+host9
+EOT
+
