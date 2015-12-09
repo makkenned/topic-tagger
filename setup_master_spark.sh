656d1a0ae17b8515854dadb08b5d6d4a0f1bf52f
a20012251 <a20012251@gmail.com>
2015-12-09 09:29:42 -0500

    Update setup_master_spark.sh

--- setup_master_spark.sh
+++ setup_master_spark.sh
@@ -1,14 +1,11 @@
 #! /bin/bash
 
 cat <<EOT >$SPARK_HOME/conf/slaves
-host2
+h1
+h2
 host3
 host4
-host5
-host6
-host7
-host8
-host9
 EOT
 
+ssh-keygen -t rsa -P "" -f /root/.ssh/id_rsa
 for i in host2 host3 host4 host5 host6 host7 host8 host9 localhost; do ssh-copy-id $i; done
