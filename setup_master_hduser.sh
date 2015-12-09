1596d23e799498f3a4828f9752e83591c2688aa3
a20012251 <a20012251@gmail.com>
2015-12-09 10:59:13 -0500

    Update setup_master_hduser.sh

--- setup_master_hduser.sh
+++ setup_master_hduser.sh
@@ -5,14 +5,12 @@ ssh-keygen -t rsa -P "" -f /home/hduser/.ssh/id_rsa
 for i in host2 host3 host4 host5 host6 host7 host8 host9 localhost; do ssh-copy-id $i; done
 
 cat <<EOT >/usr/local/hadoop/etc/hadoop/slaves
-host2
 host3
 host4
-host5
-host6
-host7
-host8
-host9
+h1
+h2
+h3 
+h4
 EOT
 
 hdfs namenode -format
