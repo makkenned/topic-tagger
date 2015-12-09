42cf5ca62cce8670d8b05dd968a8d4025e78f81e
a20012251 <a20012251@gmail.com>
2015-12-09 10:11:40 -0500

    Update setup_master_spark.sh

--- setup_master_spark.sh
+++ setup_master_spark.sh
@@ -9,3 +9,11 @@ EOT
 
 ssh-keygen -t rsa -P "" -f /root/.ssh/id_rsa
 for i in host2 host3 host4 host5 host6 host7 host8 host9 localhost; do ssh-copy-id $i; done
+
+cat <<EOT >> ~/.bash_profile
+export HADOOP_HOME=/usr/local/hadoop
+export HADOOP_MAPRED_HOME=\$HADOOP_HOME
+export HADOOP_HDFS_HOME=\$HADOOP_HOME
+export YARN_HOME=\$HADOOP_HOME
+export PATH=\$PATH:\$HADOOP_HOME/bin:\$HADOOP_HOME/sbin
+EOT
